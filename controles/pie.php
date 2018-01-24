17c28d94f1e2ffa50ff831b5d2d02e8d6c23fc3d
Ludwing <ludwinglaguna@gmail.com>
2018-01-23 22:43:10 -0500

    Add files via upload

---
+++ controles/pie.php
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+	window.status="Efiterm";
+</script>
+<table cellspacing="0" cellpadding="0" border="0" width="100%">
+    <tr valign="top">
+        <td align="center" valign="middle" class="pie">
+<!--			Para comentarios o solicitar información enviar e-mail a tovarlp@pdvsa.com<br>
+			Teléfonos: 93-56395 / 56403<br> -->
+			Copyright © Petróleos de Venezuela S. A. 2010
+        </td>
+    </tr>
+</table>
+
